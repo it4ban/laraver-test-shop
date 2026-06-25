9067061d5bd4a8e0d460a36b5e474001d2976ebd
Alex Yagodkin <adamsHillzzz@gmail.com>
2026-06-25 07:19:09 +0300

    feat: add new route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,36 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // $aboutPageUrl = route('about');
+    // dd($aboutPageUrl);
+
+    $productUrl = route('product.view', ['lang' => 'en', 'id' => 1]);
+    dd($productUrl);
+
     return view('welcome');
 });
 
-Route::view('/about/', 'about');
+Route::get('/user/profile', function () {})->name('profile');
+
+Route::get('/current-user/', function () {
+    // return redirect()->route('profile');
+    return to_route('profile');
+});
+
+Route::view('/about/', 'about')->name('about');
+
+Route::get('/product/{id}', function (string $id) {
+    return "Works! $id";
+})->whereAlphaNumeric('id');
+
+Route::get('/user/{username}', function (string $username) {
+    // ...
+})->where('username', '[a-z]+');
+
+Route::get('{lang}/product/{id}', function (string $lang, string $id) {
+    // ...
+})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}'])->name('product.view');
+
+Route::get('/search/{search}', function (string $search) {
+    return "$search";
+})->where('search', '.+');
